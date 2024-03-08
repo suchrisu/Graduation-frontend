332953e0d83f795c075c17b88715823a1414a50a
suchrisu <1374033672@qq.com>
2024-03-08 22:08:25 +0800

    登录界面

---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,84 @@
+<template> 
+    <div class="loginForm">
+    <el-form :label-position="labelPostion" ref="form" :model="form" >
+        <el-form-item label="登录角色">
+            <el-select v-model="form.rolePowerId" placeholder="请选择登录角色"> 
+              <el-option label="普通用户" value="2"></el-option>
+              <el-option label="管理员" value="1"></el-option>
+            </el-select>
+          </el-form-item>
+
+        <el-form-item label="用户名">
+          <el-input v-model="form.userId" placeholder="请输入注册时的邮箱" class="userIdInput"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" >
+            <el-input v-model="form.userPassword" show-password placeholder="请输入密码" class="userPasswordInput"></el-input>
+          </el-form-item>
+          <el-form-item label="验证码">
+            <el-input v-model="form.userId" placeholder="请输入验证码" class="codeInput"></el-input>
+          </el-form-item>
+        <el-form-item>
+            <el-button type="primary" round>登录</el-button>
+        </el-form-item> 
+      </el-form>
+    </div>
+</template>
+  <script>
+    export default {
+     name: "LoginForm",
+
+      data() {
+        return {
+          labelPostion:"top",
+          form: {
+            userId: '',
+            userPassword: '',
+            rolePowerId: '2',
+          }
+        }
+      },
+      methods: {
+        onSubmit() {
+          console.log('submit!');
+        }
+      }
+    }
+  </script>
+
+<style scoped>
+
+.userIdInput{
+ 
+}
+.userPasswordInput{
+
+}
+
+
+.codeInput{
+    width: 50%;
+}
+.el-select{
+    width: 40%;
+}
+.el-form-item{
+    margin-top: 15px;
+    margin-bottom: 10px;
+    position: relative;
+    left: 50%;
+    width: 85%;
+    transform: translate(-50%);
+    height: auto;
+}
+
+
+
+.el-button{
+    margin-top: 20px;
+    width: 40%;
+    position: relative;
+    left: 50%;
+    transform: translate(-50%);
+}
+
+</style>
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+     <el-row>
+        <el-col :span="12"><div class="left">
+            <img src="../../assets/loginAside.jpeg" alt="" class="img">
+        </div></el-col>
+        <el-col :span="12"><div class="right">
+              <LoginForm class="LoginForm"></LoginForm>
+        </div></el-col>
+      </el-row>
+      
+    </div>
+</template>
+
+
+<script>
+import LoginForm from '@/components/login/LoginForm.vue';
+export default{
+    name: "LoginView",
+    components:{
+        LoginForm
+    },
+    data() {
+      return {
+        labelPosition: "top",
+        formLabelAlign: {
+          name: '',
+          region: '',
+          type: ''
+        }
+      };
+    }
+}
+</script>
+
+<style scoped>
+.left{
+    width: 100%;
+    height: 100vh;
+}
+
+.right{
+    width: 100%;
+    height: 100vh;
+}
+.img{
+    width: 100%;
+    height: 100%;
+    border: 0;
+    margin: 0;
+    object-fit: cover;
+    float:left;
+}
+.LoginForm{
+    position: relative;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    width: 60%;
+    height: 80%;
+    background-color: red;
+    float: left;
+}
+</style>
+
+
