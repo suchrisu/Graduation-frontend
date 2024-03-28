7ab5b84b1b22aadef2ee30250e56a4bdfb12f19e
suchrisu <1374033672@qq.com>
2024-03-28 21:23:39 +0800

    用户管理-表格完成

--- src/views/manager/userManage/UserView.vue
+++ src/views/manager/userManage/UserView.vue
@@ -1,45 +1,95 @@
 <template>   
     <div class="content">
         <div class="top">
-            <div class="head-pic">
-              <HeadPortrait :imgUrl="userIcon"></HeadPortrait>
-            </div>
-            <div class="info-detail">
-              <div class="name">普通用户</div>
-              <div class="detail"></div>
-            </div>
+          
+          <el-input
+          v-model="searchInput"
+          style="max-width: 600px"
+          placeholder="Please input"
+          class="search"
+          size="default"
+          :prefix-icon="Search"
+        >
+          <template #prepend>
+            <el-select v-model="searchSelect" placeholder="Select" class="selectSearch" size="default">
+              <el-option label="用户名" :value="1" />
+              <el-option label="昵称" :value="2" />
+            </el-select>
+          </template>
+          <template #suffix>
+            <el-icon class="sessionSearchIcon"><Search/></el-icon>
+          </template>
+        </el-input>
+        <el-button class="deleteBatch" plain type="danger" :icon="Delete">批量删除</el-button>
+        <el-button class="add" plain type="primary" :icon="Plus">添加用户</el-button>
         </div>
     <div class="center">
     <el-table
-    ref="multipleTableRef"
+    ref="tableRef"
+    @row-click="rowClick"
     :data="tableData"
+    :highlight-current-row="true"
+    :select-on-indeterminate="false"
     style="width: 100%"
     size="large"
     table-layout="auto"
-    header-row-class-name="tableHeader"
-    :header-cell-style="test"
+    :header-cell-style="tableHeaderStyle"
+    :cell-style="tableCellStyle"
+    :row-style="rowStyle"
     @selection-change="handleSelectionChange"
+    class="table"
   >
-    <el-table-column type="selection" align="center"  />
+  <el-table-column  type="expand">
+    <template #default="scope">
+      <el-descriptions title="User Info">
+        <el-descriptions-item label="用户名">{{ scope.row.userId}}</el-descriptions-item>
+        <el-descriptions-item label="昵称">{{ scope.row.userName}}</el-descriptions-item>
+        <el-descriptions-item label="注册时间">{{ scope.row.createTime }}</el-descriptions-item>
+        <el-descriptions-item label="新密码">
+          <el-input :width="100" size="small"></el-input>
+        </el-descriptions-item>
+        <el-descriptions-item label="确认密码"
+          ><el-input :width="100" size="small"></el-input></el-descriptions-item
+        >
+      </el-descriptions>
+    </template>
+  </el-table-column>
+    <el-table-column fixed type="selection" align="center" />
     <el-table-column type="index" align="center" />
-    <el-table-column label="头像" align="center">
-        <el-avatar :src="userIcon" shape="circle" :size="35" fit="cover"></el-avatar>
+    <el-table-column label="" align="center">
+        <el-avatar :src="userIcon" shape="circle" :size="25" fit="cover"></el-avatar>
     </el-table-column>
     <el-table-column prop="userId" label="用户名" align="center">
     </el-table-column>
     <el-table-column property="userName" label="昵称"  align="center"/>
-    <el-table-column property="createTime" label="注册时间" align="center"/>
+    <el-table-column  property="createTime" label="注册时间" align="center"/>
     <el-table-column label="操作" align="center">
-        <el-button plain type="primary">修改</el-button>
-        <el-button plain type="danger">删除</el-button>
+      <template #default="scope">
+        <el-button plain type="primary" :icon="Edit" circle @click.stop="test(scope)"></el-button>
+        <el-button plain type="danger" :icon="Delete" circle @click.stop></el-button>
+      </template>
+        
     </el-table-column>
   </el-table>
+    </div>
+    <div class="bottom">
+      <el-pagination
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="[5, 10, 20, 50]"
+      :small="true"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="totalCount"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
     </div>
 </div>
 </template>
 
 <script>
 import HeadPortrait  from "@/components/HeadPortrait.vue"
+import {Plus,Edit,Delete} from "@element-plus/icons-vue"
 export default {
     name: 'UserView',
     components:{
@@ -47,8 +97,95 @@ export default {
     },
     data(){
         return{
+          Edit:Edit,
+          Delete:Delete,
+          Plus:Plus,
+          currentPage: 1,
+          pageSize: 5,
+          totalCount: 20,
+          searchInput:"",
+          searchSelect: 1,
+          tableHeaderClass: 'tableHeader',
             userIcon:require('@/assets/img/user.png'),
             tableData:[
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/13 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
+                {
+                    userId: "12345678",
+                    userName: "sususu",
+                    createTime: "2024/03/12 12:00:00",
+
+                },
                 {
                     userId: "12345678",
                     userName: "sususu",
@@ -59,10 +196,36 @@ export default {
         }
     },
     methods:{
-        test(){
-            return {
-                backgroundColor: "white"
-            }
+      test(scope){
+        console.log(scope);
+        this.$refs.tableRef.toggleRowExpansion(scope.row)
+      },
+      rowClick(row,column,event){
+        console.log(row,column,event)
+        this.$refs.tableRef.toggleRowSelection(row)
+      },
+      rowStyle(row){
+        return {
+          cursor: "pointer",
+          height: "30px",
+          borderBottomColor: "rgb(76,77,79)"
+        }
+      },
+      tableCellStyle(data){
+        if(data.columnIndex == 1){
+          return {
+          display: "None"
+        }
+        }
+        
+      },
+        tableHeaderStyle(data){
+
+            if(data.columnIndex == 1){
+          return {
+          display: "None"
+        }
+        }
         }
     }
 }
@@ -72,40 +235,73 @@ export default {
 .content{
     width: 100%;
     height: 100%;
-    margin-left: 20px;
+    margin-left: 30px;
     position: relative;
 }
 .top {
-    margin-bottom: 10px;
+    margin-bottom: 5px;
+    margin-left: 10px;
     &::after {
       content: "";
       display: block;
       clear: both;
     }
-    .head-pic {
+    .search{
+      border-left: 0px;
       float: left;
-    }
-    .info-detail {
-      float: left;
-      margin: 5px 20px 0;
-      .name {
-        font-size: 20px;
-        font-weight: 600;
-        color: #fff;
-      }
-      .detail {
-        color: #9e9e9e;
-        font-size: 12px;
-        margin-top: 2px;
+      width: 30%;
+      .selectSearch{
+        width:95px;
+        background-color:rgb(39,42,55);
+        border-right: 0px;
       }
     }
-}
 
+   
+    
+}
+.search{
+  border-left: 0px;
+  width: 70%;
+  .selectSearch{
+    width:95px;
+    background-color:rgb(39,42,55);
+    border-right: 0px;
+  }
+}
 .center{
     width: 70vw;
 }
-.tableHeader{
-   background-color: white;
+.table{
+  border-radius: 2px;
+  box-shadow: --el-box-shadow-light;
+  border-top: 0px solid;
+  border-bottom-color: #9e9e9e;
+  border-color: rgb(76,77,79);
+  height: 500px;
+  --el-table-row-hover-bg-color: rgb(50,54,68);
+  --el-table-header-text-color: white;
+  --el-table-text-color: white;
+  --el-table-bg-color: rgb(39,42,55);
+  --el-table-tr-bg-color: rgb(39,42,55);
+  --el-table-header-bg-color:  rgb(39,42,55);
 }
+.el-table {
+  
+}
+.bottom{
+  margin-top:5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+}
+.deleteBatch{
+  margin:0 5px;
+  float: right;
+}
+.add{
+  margin:0 5px;
+  float: right;
+}
 </style>
